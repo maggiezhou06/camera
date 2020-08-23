929c61df306ac56f746fd5e1918c64316bac3295
maggiezhou06 <69057964+maggiezhou06@users.noreply.github.com>
2020-08-23 15:38:48 -0400

    Update main.js

--- ubuntu_webpack_server/src/main.js
+++ ubuntu_webpack_server/src/main.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import store from './store'
 import BootstrapVue from 'bootstrap-vue' 
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faCog } from '@fortawesome/free-solid-svg-icons'
@@ -45,6 +44,5 @@ Vue.config.productionTip = false
 
 new Vue({
   router,
-  store,
   render: h => h(App),
 }).$mount('#app')
